68bb1811d7d91e2fcfa9c145ed55221f98a879dc
Abderahmen Abderazak <abderahmenabderazak@gmail.com>
2024-11-29 14:40:03 +0100

    added microsoft clarity

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,12 @@ for (let i = 0; i < navbarLinks.length; i++) {
 
 
 
-
+/*clarity*/
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "p65rllr1wz");
 
 /**
  * back to top & header
